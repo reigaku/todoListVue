f240e06f6188e8dd87131bfff23e4b47775c57b9
reigaku <74679900+reigaku@users.noreply.github.com>
2021-06-07 17:07:31 +0300

    added all necessary files

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,48 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <input type="text" v-model="title">
+        <button type="submit">create</button>
+    </form>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        onSubmit(){
+            if (this.title.trim()) {
+                const newTodo = {
+                    id: Date.now(),
+                    title: this.title,
+                    completed: false
+                }
+
+                this.$emit('add-todo', newTodo)
+                this.title = ''
+            }
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    input{
+        border: 1px solid #ccc;
+        padding: .5rem 2rem;
+        margin-bottom: 1rem;
+        margin-right: 1rem;
+    }
+
+    button{
+        background: green;
+        color: white;
+        border: 1px solid #ccc;
+        padding: .5rem 2rem;
+        
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h2>home page</h2>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab reprehenderit corporis et odio provident consectetur mollitia incidunt! Sint placeat autem facilis dolores, fuga quaerat necessitatibus quam facere iure error ducimus.
+    </p>
+    <router-link to="/todos">Todos</router-link>
+    </div>
+    
+</template>
